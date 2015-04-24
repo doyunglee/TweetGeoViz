7de3cea58b6df48419aa5563da12709053a53761
jagreene <j.austin.greene@gmail.com>
2015-04-24 08:02:52 -0400

    small fix

--- tweeting.py
+++ tweeting.py
@@ -33,9 +33,11 @@ def tweeting(epi,r1,r2,tI,tF):
     db = client['twitter']
     collection = db['ControlTweets']
     #tlt is tweet latitude, which is around 31, tln is longitude, which is around -91
-    start = tI
-    end = tF
+    #start = tI
+    #end = tF\
 
+    start = datetime.datetime(2014,7,1,0,0,0,0)
+    end = datetime.datetime(2014,11,15,0,0,0,0)
     #this finds all the tweets under the selected criteria. results is all the tweets in the outbreak zone. wide_area_results is the area around it.
     #results = collection.find({'cc': 'US' , 'tlt': {"$gt": epi-r1, "$lt": epi+r1 }, 'tln': {"$gt": epi-r1, "$lt": epi+r1 }, 'cr': {'$gt': start, '$lt': end}}, limit=10000)
     #wide_area_results = collection.find({'cc': 'US' , 'tlt': {"$gt": epi-r2, "$lt": epi+r2}, 'tln': {"$gt": epi-r2, "$lt": epi+r2 }, 'cr': {'$gt': start, '$lt': end}}, limit=10000)
